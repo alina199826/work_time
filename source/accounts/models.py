f5e53f7cb38240874d3f52e771d7d2e63759d468
Alina <gartok.alina@gmail.com>
2023-04-25 19:14:23 +0600

    убрала хранение токенов из базы данных

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -3,6 +3,9 @@ from django.core.validators import RegexValidator
 from django.db import models
 from webapp.models import Organization
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from datetime import datetime, timedelta
+import jwt
+
 
 
 class UserManager(BaseUserManager):
@@ -53,5 +56,26 @@ class User(AbstractBaseUser):
 
 class VerificationToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_token", verbose_name="Работник")
-    token = models.CharField(max_length=255, verbose_name="Токен")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время и дата создания")
+
+    @property
+    def token(self):
+        payload = {
+            'user_id': self.user.id,
+            'exp': datetime.utcnow() + timedelta(days=1),
+            'iat': datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'mysecret', algorithm='HS256')
+        return token
+
+    @classmethod
+    def verify_token(cls, token):
+        try:
+            payload = jwt.decode(token, 'mysecret', algorithms=['HS256'])
+            user = User.objects.get(id=payload['user_id'])
+            return user
+        except jwt.ExpiredSignatureError:
+            pass
+        except jwt.InvalidTokenError:
+            pass
+        return None
